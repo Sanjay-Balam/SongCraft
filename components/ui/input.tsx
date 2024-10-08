88b130743c7f0ed9500e8d79ae25310fd4d04fd6
Sanjay Balam <b.sanjay0701@gmail.com>
2024-10-08 21:59:41 +0530

    update changes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "text", ...props }, ref) => { // Set a default type to avoid undefined
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, type = "text", ...props }, ref) => {
     return (
       <input
         type={type}
